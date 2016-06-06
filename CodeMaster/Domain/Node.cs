c263808802c6219d4ea5d332887ec1535919b637
Deepak Khot <dk4893@yahoo.co.in>
2016-06-06 21:13:20 +0530

    Code master 1st draft

---
+++ CodeMaster/Domain/Node.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeMaster.Domain
+{
+    class Node
+    {
+        public int NodeId { get; set; }
+    }
+
+    class NodeBinding
+    {
+        public Node SoruceNode { get; set; }
+        public Node DestinationNode { get; set; }
+        public Color ConnectedByToken { get; set; }
+    }
+
+    class Map
+    {
+        public List<NodeBinding> NodeBindings { get; set; }
+        public Node StartNode { get; set; }
+        public Node EndNode { get; set; }
+    }
+
+    enum Color
+    {
+        Green = 1,
+        Red = 2,
+        Blue = 3
+    }
+
+}
