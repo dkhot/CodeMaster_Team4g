1d8691e04f548e916d51f38dbbab8f3436fed251
Deepak Khot <dk4893@yahoo.co.in>
2016-06-06 21:51:39 +0530

    Added Edges class and a method to create edges

--- CodeMaster/Domain/Node.cs
+++ CodeMaster/Domain/Node.cs
@@ -1,35 +1,95 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CodeMaster.Domain
 {
-    class Node
+    public class Edge
     {
-        public int NodeId { get; set; }
-    }
-
-    class NodeBinding
-    {
-        public Node SoruceNode { get; set; }
         public Node DestinationNode { get; set; }
-        public Color ConnectedByToken { get; set; }
+        public Color ConnectedWith { get; set; }
+
     }
 
-    class Map
+    public enum Color
     {
-        public List<NodeBinding> NodeBindings { get; set; }
-        public Node StartNode { get; set; }
-        public Node EndNode { get; set; }
-    }
-
-    enum Color
-    {
+        Red = 0,
         Green = 1,
-        Red = 2,
-        Blue = 3
+        Blue = 2
+    }
+
+    public class Node
+    {
+        public int Id { get; set; }
+        public List<Edge> Edges { get; set; }
+
+    }
+
+
+    public class Map
+    {
+        private readonly List<Node> _nodes = new List<Node>();
+        private const int NumberOfNodes = 6;
+
+        public List<Node> Nodes
+        {
+            get { return _nodes; }
+        }
+
+        public Map()
+        {
+            for (var i = 0; i < NumberOfNodes; i++)
+            {
+                _nodes.Add(CreateNode(i));
+            }
+            CreateEdge(0, 1, Color.Red);
+            CreateEdge(1, 0, Color.Red);
+            CreateEdge(0, 2, Color.Green);
+            CreateEdge(2, 0, Color.Green);
+            CreateEdge(0, 4, Color.Blue);
+            CreateEdge(4, 0, Color.Blue);
+            CreateEdge(1, 2, Color.Blue);
+            CreateEdge(2, 1, Color.Blue);
+            CreateEdge(1, 3, Color.Green);
+            CreateEdge(3, 1, Color.Green);
+            CreateEdge(3, 4, Color.Red);
+            CreateEdge(4, 3, Color.Red);
+            CreateEdge(3, 5, Color.Blue);
+            CreateEdge(5, 3, Color.Blue);
+            CreateEdge(4, 5, Color.Green);
+            CreateEdge(5, 4, Color.Green);
+
+        }
+
+        private static Node CreateNode(int id)
+        {
+            return new Node()
+            {
+                Id = id,
+                Edges = new List<Edge>()
+            };
+        }
+
+        public void CreateEdge(int sourceNodeId, int connectedNodeId, Color connectedByColor)
+        {
+            if (!IsNodeExists(sourceNodeId) || !IsNodeExists(connectedNodeId)) return;
+            var connectedNode = _nodes.First(x => x.Id == connectedNodeId);
+            var sourceNode = _nodes.First(x => x.Id == sourceNodeId);
+
+            sourceNode.Edges.Add(new Edge
+            {
+                DestinationNode = connectedNode,
+                ConnectedWith = connectedByColor
+
+            });
+        }
+
+        private bool IsNodeExists(int id)
+        {
+            return _nodes.Any(x => x.Id == id);
+
+        }
+
     }
 
 }
+
